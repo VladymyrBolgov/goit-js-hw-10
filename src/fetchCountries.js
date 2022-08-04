e461655a6e62c40418e1e698af605c5dc39b8fb3
VladymyrBolgov <vladymyr.bolgov@gmail.com>
2022-08-04 21:32:47 +0300

    1

---
+++ src/fetchCountries.js
@@ -0,0 +1,7 @@
+
+export function fetchCountries(name) {
+    const url = `https://restcountries.com/v3.1/all?fields=name,capital,population,flags,languages` 
+    
+    return url;
+}
+
--- src/index.js
+++ src/index.js
@@ -1,5 +1,24 @@
 import './css/styles.css';
-
+import debounce from 'lodash.debounce';
 import Notiflix from 'notiflix';
 
+import { fetchCountries } from './fetchCountries';
+
 const DEBOUNCE_DELAY = 300;
+
+const input = document.querySelector('#search-box');
+const countryList = document.querySelector('.country-list');
+const countryInfo = document.querySelector('.country-info');
+
+//input.addEventListener('input', debounce(searchCountries, DEBOUNCE_DELAY));
+
+
+
+const searchCountries = (event => {
+    event.preventDefault();
+
+    const url = fetchCountries();
+
+
+})
+
