f2c75bc874211ca343e7de45253f922eb0077895
VladymyrBolgov <vladymyr.bolgov@gmail.com>
2022-08-04 21:53:10 +0300

    l

--- src/index.js
+++ src/index.js
@@ -10,15 +10,19 @@ const input = document.querySelector('#search-box');
 const countryList = document.querySelector('.country-list');
 const countryInfo = document.querySelector('.country-info');
 
-//input.addEventListener('input', debounce(searchCountries, DEBOUNCE_DELAY));
-
-
+input.addEventListener('input', debounce(searchCountries, DEBOUNCE_DELAY));
 
 const searchCountries = (event => {
     event.preventDefault();
 
     const url = fetchCountries();
-
+    
+    fetch(url)
+        .then((response) => response.json())
+    
+        .catch((error) => {
+            console.log("error", error);
+          });
 
 })
 
