3d96d7e6e56ee681df8c79c19c3740267b698744
VladymyrBolgov <vladymyr.bolgov@gmail.com>
2022-08-06 23:28:17 +0300

    b

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -1,1 +1,7 @@
 
+export function fetchCountries(name) {
+    const url = `https://restcountries.com/v3.1/name/${name}?fields=name,capital,flags,population,languages`; 
+    
+    return fetch(url)
+        .then((response) => response.json());
+}
---
+++ src/index.js
@@ -0,0 +1,64 @@
+import './css/styles.css';
+import debounce from 'lodash.debounce';
+import Notiflix from 'notiflix';
+
+import { fetchCountries } from './fetchCountries';
+
+const DEBOUNCE_DELAY = 300;
+
+const input = document.querySelector('#search-box');
+const countryList = document.querySelector('.country-list');
+const countryInfo = document.querySelector('.country-info');
+
+const searchCountries = (event) => {
+    event.preventDefault();
+
+    const searchValue = input.value;
+    let spaceSearchValue = searchValue.trim();
+
+    if (spaceSearchValue === '') {
+        removeHTML()
+        Notiflix.Notify.info('Пожалуйста, введите имя');
+        return
+    }
+
+    fetchCountries(spaceSearchValue)
+        .then(renderСountryCard)
+        .catch(ifFetchError)
+}
+input.addEventListener('input', debounce(searchCountries, DEBOUNCE_DELAY));
+
+function renderСountryCard (countries) {
+    const size = Object.keys(countries).length;
+    removeHTML();
+    for (const country of countries) {
+        const { flags, name, capital, population, languages } = country;
+
+        const createElementSymbols =
+            ``
+
+
+        const createElementInfo =
+            ``
+
+        if (size > 10) {
+            Notiflix.Notify.info('Too many matches found. Please enter a more specific name.');
+            return
+        }
+        visualization(size, createElementSymbols, createElementInfo)
+    }
+}
+
+function visualization(size, createElementSymbols, createElementInfo) {
+    if (size <= 1) {
+        countryList.insertAdjacentHTML('beforeend', createElementSymbols);
+        countryInfo.insertAdjacentHTML('beforeend', createElementInfo);
+    }
+    else {
+        countryList.insertAdjacentHTML('beforeend', createElementSymbols);
+    }
+}
+
+function ifFetchError(error) {
+    Notiflix.Notify.failure('Oops, there is no country with that name');
+}
